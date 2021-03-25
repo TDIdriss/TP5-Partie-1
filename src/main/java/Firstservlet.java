cb591281cab1a44ba6947c7ee4b204ec92923ade
Traore Idriss <tdahabaidriss@gmail.com>
2021-03-25 01:30:35 +0100

    debut

---
+++ src/main/java/Firstservlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="mytest",
+        urlPatterns={"/myurl"})
+public class Firstservlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter p = new PrintWriter(resp.getOutputStream());
+        p.print("Hello world");
+        p.flush();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+}
